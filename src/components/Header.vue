203b9fb8e113ccee5974268b83850423cb7ab54a
MuhamadPrajitno <muhamadismuaji@gmail.com>
2017-09-16 18:59:27 +0700

    deploying in progress

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,14 @@
 <template>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-		<router-link to="/" tag="a" class="navbar-brand" exact>Stock Trader (Funds: {{ funds | currency}})</router-link>
+		<router-link to="/stock-trader" tag="a" class="navbar-brand" exact>Stock Trader (Funds: {{ funds | currency}})</router-link>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
 			<ul class="navbar-nav mr-auto">
-				<router-link to="/portofolio" tag="li" class="nav-item" active-class="active"><a class="nav-link">Portofolio</a></router-link>
-				<router-link to="/stocks" tag="li" class="nav-item" active-class="active"><a class="nav-link">Stocks</a></router-link>
+				<router-link to="/stock-trader/portofolio" tag="li" class="nav-item" active-class="active"><a class="nav-link">Portofolio</a></router-link>
+				<router-link to="/stock-trader/stocks" tag="li" class="nav-item" active-class="active"><a class="nav-link">Stocks</a></router-link>
 				<li class="nav-item"><a href="#" class="nav-link" @click="endDay">End Day</a></li>
 			</ul>
 			<!-- <span class="navbar-text">
