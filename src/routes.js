10b6f97e3e5b73d125497ab3ec2bf99028544f25
MuhamadPrajitno <muhamadismuaji@gmail.com>
2017-09-16 19:07:11 +0700

    deploying in progress

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ import Portofolio from './components/portofolio/Portofolio.vue'
 import Stocks from './components/stocks/Stocks.vue'
 
 export const routes = [
-	{path: '/stock-trader', component: Home},
-	{path: '/stock-trader/portofolio', component: Portofolio},
-	{path: '/stock-trader/stocks', component: Stocks},
+	{path: '/stock-trader', name: 'home', component: Home},
+	{path: '/stock-trader/portofolio', name: 'portofolio', component: Portofolio},
+	{path: '/stock-trader/stocks', name: 'stocks', component: Stocks},
+	{path: '*', redirect: {name: 'home'}}
 ];
